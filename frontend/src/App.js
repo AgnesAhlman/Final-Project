82f06aeb699e1f253b3bccd7dd6c9ef941bd3af8
Kim Svensson <110751174+Earnieball@users.noreply.github.com>
2022-12-12 13:52:41 +0100

    Removed some code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from 'components/Main';
-import Login from 'components/Login';
-import NotFound from 'components/NotFound';
-import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import thoughts from 'reducers/thoughts';
-import user from 'reducers/user';
+// import { Provider } from 'react-redux';
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 
-const reducer = combineReducers({
-  user: user.reducer,
-  thoughts: thoughts.reducer
-});
-const store = configureStore({reducer});
+
+// const reducer = combineReducers({
+//   user: user.reducer,
+//   thoughts: thoughts.reducer
+// });
+// const store = configureStore({reducer});
 export const App = () => {
   return (
-    <Provider store={store}>
+    // <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login/>}></Route>
-          <Route path='/' element={<Main/>}></Route>
-          <Route path='*' element={<NotFound/>}></Route>
+        <Route></Route>
         </Routes>
       </BrowserRouter>
-    </Provider>
+    // </Provider>
       
   );
 }
