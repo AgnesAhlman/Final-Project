525d1051a78b197404dc0a066bb9506dd361ba6f
Kim Svensson <110751174+Earnieball@users.noreply.github.com>
2022-12-12 17:48:08 +0100

    nu då

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import crypto from "crypto";
-import bcrypt from "bcrypt";
+import PosterData from "./data/posters.json"
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,148 +16,40 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+const Poster = mongoose.model("Poster", {
+  posterID: Number,
+  group: Number,
+  title: String,
+  color: String,
+  description: String,
+  img: String,
+  price: String,
+  size: String 
+});
+
 /////// Monday
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-    /// My_B4nK_P4$$word
-  },
-  // npm install crypto
-  accessToken: {
-    type: String,
-    default: () => crypto.randomBytes(128).toString("hex")
-  }
-});
+// const PosterSchema = new mongoose.Schema({
+//  poster: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   }
 
-const User = mongoose.model("User", UserSchema);
+// });
 
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-// npm install bcrypt
-// const code = [1, 2, 4, 4];
-// const makeCodeSecret = (codeArr) => {
-    // const secretMessage = codeArr.map(singleNumber => singleNumber + 1);
-    // return secretMessage
-//}
-// transformedCode = makeCodeSecret(code)
-  try {
-    const salt = bcrypt.genSaltSync();
-    if (password.length < 8) {
-      res.status(400).json({
-        success: false,
-        response: "Password must be at least 8 characters long"
-      });
-    } else {
-      const newUser = await new User({username: username, password: bcrypt.hashSync(password, salt)}).save();
-      res.status(201).json({
-        success: true,
-        response: {
-          username: newUser.username,
-          accessToken: newUser.accessToken,
-          id: newUser._id
-        }
-      });
-    }
-  } catch(error) {
-      res.status(400).json({
-        success: false,
-        response: error
-      });
-  }
-});
-
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await User.findOne({username});
-    if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(200).json({
-        success: true,
-        response: {
-          username: user.username,
-          id: user._id,
-          accessToken: user.accessToken
-        }
-      });
-    } else {
-      res.status(400).json({
-        success: false,
-        response: "Credentials didn't match"
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      response: error
-    });
-  }
-});
-
-const authenticateUser = async (req, res, next) => {
-  const accessToken = req.header("Authorization");
-  try {
-    const user = await User.findOne({accessToken: accessToken});
-    if (user) {
-      next();
-    } else {
-      res.status(401).json({
-        response: "Please log in",
-        success: false
-      })
-    }
-  } catch (error) {
-    res.status(400).json({
-      response: error,
-      success: false
-    })
-  }
-}
-
-const ThoughtSchema = new mongoose.Schema({
-  message: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: () => new Date() 
-  },
-  hearts: {
-    type: Number,
-    default: 0
-  }
-}); 
-
-
-const Thought = mongoose.model("Thought", ThoughtSchema);
-
-app.get("/thoughts", authenticateUser);
-app.get("/thoughts", async (req, res)=> {
-  const thoughts = await Thought.find({});
-  res.status(200).json({success: true, response: thoughts});
-});
-
-app.post("/thoughts", authenticateUser)
-app.post("/thoughts", async (req, res) => {
-  const { message } = req.body;
-  try {
-    const newThought = await new Thought({message}).save();
-    res.status(201).json({success: true, response: newThought});
-  } catch (error) {
-    res.status(400).json({success: false, response: error});
-  }
-});
-
+// const Poster = mongoose.model("Posters", PosterSchema);
 ///////
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/posters", async (req, res) => {
+  const posters = await Poster.find().then(posters =>{
+    res.json(posters)
+  })
+   
 });
 
 // Start the server
