5485188c695877545fc726fedd29764232be1db3
Kim Svensson <110751174+Earnieball@users.noreply.github.com>
2023-01-16 10:01:34 +0100

    Dropdown menu

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,23 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Links from '../elements/Links';
 import StyledNavbar from '../blocks/Navbar';
+import Dropdown from './Dropdown';
 
 const Navbar = (props) => {
   return (
-    <StyledNavbar shadow={props.shadow} primary={props.primary}>
-      <Links to="/">Home</Links>
-      <Links to="/posters">Posters</Links>
-      <Links to="/login">
-        <StyledNavbar.UserIcon src="/user.svg" alt="user-icon" />
-      </Links>
-      <Links to="/checkout">
-        <StyledNavbar.Icon src="/cartIcon.svg" alt="cartIcon" />
-      </Links>
-    </StyledNavbar>
+    <>
+      <Dropdown />
+      <StyledNavbar shadow={props.shadow} primary={props.primary}>
+        <Links to="/">Home</Links>
+        <Links to="/posters">Posters</Links>
+        <Links to="/login">
+          <StyledNavbar.UserIcon src="/user.svg" alt="user-icon" />
+        </Links>
+        <Links to="/checkout">
+          <StyledNavbar.Icon src="/cartIcon.svg" alt="cartIcon" />
+        </Links>
+      </StyledNavbar>
+    </>
   );
 };
 
