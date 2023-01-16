86658e82a5ceaaa617717f386364ae6583df544c
Kim Svensson <110751174+Earnieball@users.noreply.github.com>
2023-01-16 15:06:12 +0100

    dropdownCard

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useCartProducts from 'hooks/useCartProducts';
 
 import Links from '../elements/Links';
 import StyledNavbar from '../blocks/Navbar';
 import Dropdown from './Dropdown';
+import CartCard from './pages/CartCard';
 
 const Navbar = (props) => {
-  const { totalCartItems } = useCartProducts();
+  const { totalCartItems, cartProducts } = useCartProducts();
+  const [showCard, setShowCard] = useState(false);
+
   return (
     <>
       <Dropdown />
@@ -16,8 +19,14 @@ const Navbar = (props) => {
         <Links to="/login">
           <StyledNavbar.UserIcon src="/user.svg" alt="user-icon" />
         </Links>
-        <Links to="/checkout" badge={totalCartItems}>
+        <Links
+          to="/checkout"
+          badge={totalCartItems}
+          onMouseEnter={() => setShowCard(true)}
+          onMouseLeave={() => setShowCard(false)}
+        >
           <StyledNavbar.Icon src="/cartIcon.svg" alt="cartIcon" />
+          {showCard && <CartCard products={cartProducts} />}
         </Links>
       </StyledNavbar>
     </>
