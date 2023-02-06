6f7ab4f1a5e10b1eeb75ffe6d1ecadddb62096dd
Agnes Ahlman <ahlman.agnes@gmail.com>
2023-02-06 15:01:33 +0100

    added lottie loading

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Lottie from 'lottie-react';
+import loading from '../assets/loading.json';
+
+const Loading = () => {
+  return <Lottie animationData={loading} color="000000" />;
+};
+
+export default Loading;
--- frontend/src/components/pages/AllPostersPage.js
+++ frontend/src/components/pages/AllPostersPage.js
@@ -1,10 +1,10 @@
 import Footer from 'components/Footer';
 import { Wrapper } from 'elements/Wrapper';
-
 import React from 'react';
 import { Background } from 'elements/Background';
 import { Title } from 'elements/Title';
 import { useSelector } from 'react-redux';
+import Loading from 'components/Loading';
 import Navbar from '../Navbar';
 import Posters from '../Posters';
 
@@ -16,7 +16,8 @@ const AllPostersPage = () => {
       <Background padding="10rem">
         <Wrapper center bgColor="white" padding="0rem 8rem">
           <Title>POSTERS</Title>
-          {isLoading && <p>LOADING</p>}
+
+          {isLoading && <Loading />}
           <Posters />
         </Wrapper>
       </Background>
